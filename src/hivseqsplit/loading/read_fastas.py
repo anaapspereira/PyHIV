8b3eb706fbb7aab14b649f2da83b66ccd59cb041
João Correia <jfscorreia95@gmail.com>
2025-01-25 15:15:12 +0000

    [ADD] use pathlib for path management

--- src/hivseqsplit/loading/read_fastas.py
+++ src/hivseqsplit/loading/read_fastas.py
@@ -1,18 +1,17 @@
 import logging
-import os
 from pathlib import Path
 from typing import List, Tuple
 
 from Bio import SeqIO
 
 
-def read_input_fastas(input_folder: str) -> List[Tuple[str, str]]:
+def read_input_fastas(input_folder: Path) -> List[Tuple[str, str]]:
     """
     Reads FASTA files from a specified input folder.
 
     Parameters
     ----------
-    input_folder: str
+    input_folder: Path
         Path to the folder containing the FASTA files.
 
     Returns
@@ -27,21 +26,18 @@ def read_input_fastas(input_folder: str) -> List[Tuple[str, str]]:
     ValueError
         If a FASTA file cannot be parsed.
     """
-    # TODO: handle logging properly
-    input_folder = Path(input_folder)
     if not input_folder.is_dir():
         raise NotADirectoryError(f"Input folder {input_folder} is not a directory.")
 
     sequences = []
     fasta_files = [file for file in input_folder.iterdir() if file.suffix == '.fasta']
     for file in fasta_files:
-        file_path = os.path.join(input_folder, file)
         try:
-            for record in SeqIO.parse(file_path, 'fasta'):
-                # sequences.append((record.id, str(record.seq)))
+            for record in SeqIO.parse(file, 'fasta'):
                 sequences.append(record)
                 logging.info(f"Read sequence {record.id}")
         except Exception as e:
-            logging.error(f"Error reading {file_path}: {e}")
-            raise ValueError(f"Failed to parse {file_path}") from e
+            logging.error(f"Error reading {file}: {e}")
+            raise ValueError(f"Failed to parse {file}") from e
     return sequences
+
