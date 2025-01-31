444b74649e15e6b23ad3197a8323b208f11813cf
João Correia <jfscorreia95@gmail.com>
2025-01-31 14:34:19 +0000

    [ADD] tests

---
+++ tests/unit_tests/align/test_alignments.py
@@ -0,0 +1,18 @@
+from unittest import TestCase, skip
+
+from Bio import SeqIO
+
+from hivseqsplit import align_with_references
+from tests import TEST_DIR
+
+
+@skip('mafft not installed in actions!')
+class TestAlignments(TestCase):
+
+    def test_align_with_references(self):
+        test_sequence = TEST_DIR / 'data' / 'fastas' / '2636813482.fasta'
+        references_dir = TEST_DIR / 'data' / 'references'
+        test_sequence = SeqIO.read(test_sequence, "fasta")
+        best_alignment = align_with_references(test_sequence, references_dir)
+        self.assertEqual(len(best_alignment), 3)
+
