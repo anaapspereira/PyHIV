1b011b3665a51d7aff2f9a7bf4c79d8839511656
João Correia <jfscorreia95@gmail.com>
2025-10-16 13:59:55 +0100

    [UPDATE] sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,29 +3,29 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'PyHIV'
-copyright = '2025, Ana Santos-Pereira; Joao Correia'
 author = 'Ana Santos-Pereira; Joao Correia'
 release = '0.0.3'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # For Google/NumPy-style docstrings
-    'sphinx.ext.viewcode',
-    'myst_parser',          # Support for Markdown files
+    'sphinx.ext.napoleon',       # for Google/NumPy docstrings
+    'sphinx.ext.viewcode',       # links to source code
+    'sphinx.ext.autosummary',    # auto-generate module summaries
+    'myst_parser',               # Markdown support if needed
 ]
 
-# -- Autodoc settings --------------------------------------------------------
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Autodoc settings
 autodoc_default_options = {
     'members': True,
     'undoc-members': True,
@@ -33,13 +33,5 @@ autodoc_default_options = {
 }
 autodoc_typehints = "description"
 
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# Autosummary settings
+autosummary_generate = True
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,17 +1,20 @@
 .. PyHIV documentation master file, created by
-   sphinx-quickstart on Wed Oct  8 15:30:30 2025.
+   sphinx-quickstart on Thu Oct 16 13:54:45 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-PyHIV documentation
+PyHIV Documentation
 ===================
+
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    Introduction <manual_docs/PyHIV>
-
    Command Line Interface <manual_docs/CLI>
 
-   modules
+   pyhiv
+   pyhiv.align
+   pyhiv.loading
+   pyhiv.split
